25a33ea4ae5b15b3195f4fb8163ccb4415e2079c
John P. Baugh <jpbaugh@umich.edu>
2020-06-15 19:25:38 -0400

    Add files via upload

---
+++ proj9-2-dynamic-rectangle-file/Rectangle.cpp
@@ -0,0 +1,42 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle()
+{
+	this->length = 1.0;
+	this->width = 1.0;
+}
+
+Rectangle::Rectangle(double length, double width)
+{
+	this->length = length;
+	this->width = width;
+}
+
+double Rectangle::getLength() const
+{
+	return length;
+}
+double Rectangle::getWidth() const
+{
+	return width;
+}
+
+void Rectangle::setLength(double length)
+{
+	this->length = length;
+}
+
+void Rectangle::setWidth(double width)
+{
+	this->width = width;
+}
+
+double Rectangle::area() const
+{
+	return length * width;
+}
+
+double Rectangle::perimeter() const
+{
+	return 2 * length + 2 * width;  
+}
---
+++ proj9-2-dynamic-rectangle-file/Rectangle.h
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle
+{
+	public:
+		Rectangle();
+		Rectangle(double length, double width);
+		double getLength() const;
+		double getWidth() const;
+		void setLength(double length);
+		void setWidth(double width);
+		double area() const;
+		double perimeter() const;
+
+	private:
+		double length;
+		double width;
+};
+
+#endif 
