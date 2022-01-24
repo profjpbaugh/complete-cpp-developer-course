1ee4c193155257db8b6f8f6ba891f5eb54aeb58c
Thirumalaivasan <thirumalaithiruvasan@gmail.com>
2022-01-24 16:54:36 +0530

    added const between peek and isEmpty method

--- course-source-files/section 12/LinkedStackApp/Stack.h
+++ course-source-files/section 12/LinkedStackApp/Stack.h
@@ -6,8 +6,8 @@ class Stack
 	public:
 		virtual void push(int newEntry) = 0;
 		virtual int pop() = 0;
-		virtual int peek() = 0;
-		virtual bool isEmpty() = 0;
+		virtual int peek() const = 0;
+		virtual bool isEmpty() const = 0;
 		virtual void makeEmpty() = 0;
 };
 
